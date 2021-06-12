78328e73ee52ca49abf56ac08e885353506a200f
Ansar Nisar <ansarnisargill@gmail.com>
2021-06-12 23:15:13 +0500

    setup navigation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<slot />
+<section class="hero is-fullheight">
+	<div class="hero-head">
+		<Navbar />
+	</div>
+
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<div class="column is-6 is-offset-3">
+				<slot />
+			</div>
+		</div>
+	</div>
+</section>
---
+++ src/routes/result.svelte
@@ -0,0 +1,90 @@
+<script context="module">
+	export const prerender = true;
+	export async function load({ page }) {
+		let url = page.query.get('url');
+		return {
+			props: {
+				url: url
+			}
+		};
+	}
+</script>
+
+<script>
+	export let url;
+	let isLoading = true;
+	import { onMount, tick } from 'svelte';
+	import { goto } from '$app/navigation';
+	import Loader from '../components/Loader.svelte';
+	import { GetWebsiteSecurityScore } from '../requests/SecurityScanRequests';
+	onMount(async () => {
+		await GetResults();
+	});
+	async function GetResults() {
+		let response = await GetWebsiteSecurityScore(url);
+		isLoading = false;
+		await createGaugeWidget(response.score);
+	}
+	async function createGaugeWidget(score) {
+		await tick();
+		var opts = {
+			angle: 0, // The span of the gauge arc
+			lineWidth: 0.2, // The line thickness
+			radiusScale: 1, // Relative radius
+			pointer: {
+				length: 0.6, // // Relative to gauge radius
+				strokeWidth: 0.035, // The thickness
+				color: '#000000' // Fill color
+			},
+			limitMax: false, // If false, max value increases automatically if value > maxValue
+			limitMin: false, // If true, the min value of the gauge will be fixed
+			colorStart: '#6F6EA0', // Colors
+			colorStop: '#C0C0DB', // just experiment with them
+			strokeColor: '#EEEEEE', // to see which ones work best for you
+			generateGradient: true,
+			highDpiSupport: true, // High resolution support,
+			staticLabels: {
+				font: '10px sans-serif', // Specifies font
+				labels: [0, 40, 70, 100, score], // Print labels at these values
+				color: '#000000', // Optional: Label text color
+				fractionDigits: 0 // Optional: Numerical precision. 0=round off.
+			},
+			staticZones: [
+				{ strokeStyle: '#F03E3E', min: 0, max: 40 },
+				{ strokeStyle: '#FFDD00', min: 40, max: 70 },
+				{ strokeStyle: '#30B32D', min: 70, max: 100 }
+			]
+		};
+		var target = document.getElementById('scanMeter');
+		var gauge = new Gauge(target).setOptions(opts);
+		gauge.maxValue = 100;
+		gauge.setMinValue(0);
+		gauge.animationSpeed = 32;
+		gauge.set(score);
+	}
+	async function handleNext() {
+		await goto('/');
+	}
+	async function handleBack() {
+		await goto('/');
+	}
+</script>
+
+{#if isLoading}
+	<Loader />
+{:else}
+	<canvas id="scanMeter" class="m-4" />
+	<br />
+	<button class="button m-4 is-success is-outlined is-large" on:click={handleNext}>
+		<span> Absichern und weiter Testen lassen durch Profies</span>
+		<span class="icon is-medium">
+			<i class="fas fa-check" />
+		</span>
+	</button>
+	<button class="button m-4 is-danger is-outlined is-large" on:click={handleBack}>
+		<span> Kostenlos selber weiter Testen</span>
+		<span class="icon is-medium">
+			<i class="fas fa-times" />
+		</span>
+	</button>
+{/if}
