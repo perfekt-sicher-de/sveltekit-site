efec4d68e2690c3c0f774c3c1e5c5dc78862f9dd
Ansar Nisar <ansarnisargill@gmail.com>
2021-06-27 00:09:50 +0500

    Added off-canvas menu

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,37 +2,33 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import stringResources from '../stringResources';
-	onMount(() => {
-		document.querySelector('.navbar-burger').addEventListener('click', () => {
-			document.querySelector('.navbar-burger').classList.toggle('is-active');
-			document.getElementById('navbarMenu').classList.toggle('is-active');
-		});
-	});
+	onMount(() => {});
 	async function gotoHome() {
 		await goto('/');
-		document.querySelector('.navbar-burger').click();
+		closeNav();
 	}
 	async function gotoImpressum() {
 		await goto('/impressum');
-		document.querySelector('.navbar-burger').click();
+		closeNav();
 	}
 	async function gotoDatenschutz() {
 		await goto('/datenschutz');
-		document.querySelector('.navbar-burger').click();
+		closeNav();
+	}
+	function openNav() {
+		document.getElementById('myNav').style.width = '100%';
+	}
+
+	function closeNav() {
+		document.getElementById('myNav').style.width = '0%';
 	}
 </script>
 
-<nav class="navbar">
-	<div class="container">
-		<div class="navbar-brand">
-			<span class="navbar-burger burger" data-target="navbarMenu">
-				<span />
-				<span />
-				<span />
-			</span>
-		</div>
-		<div id="navbarMenu" class="navbar-menu is-desktop">
-			<div class="navbar">
+<div id="myNav" class="overlay">
+	<h1 href="javascript:void(0)" class="closebtn" on:click={closeNav}>&times;</h1>
+	<div class="overlay-content">
+		<div class="columns m-4">
+			<div class=" column is-half is-offset-3 p-4">
 				<span class="navbar-item">
 					<button class="button is-outlined" on:click={gotoHome}>
 						<span class="icon">
@@ -60,7 +56,8 @@
 			</div>
 		</div>
 	</div>
-</nav>
+</div>
+<span style="font-size:50px;cursor:pointer" class="p-4" on:click={openNav}>&#9776;</span>
 
 <style>
 	.navbar-item .button {
@@ -73,12 +70,42 @@
 		background-color: var(--brandColor) !important;
 		color: var(--background) !important;
 	}
-	.navbar-menu {
-		background-color: transparent !important;
-		box-shadow: none !important;
-		padding: 0 !important;
+	.overlay {
+		height: 100%;
+		width: 0;
+		position: fixed;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		background-color: rgb(0, 0, 0);
+		background-color: rgba(0, 0, 0, 0.9);
+		overflow-x: hidden;
+		transition: 0.5s;
 	}
-	.navbar-burger{
-		margin-left: 0;
+
+	.overlay-content {
+		position: relative;
+		top: 25%;
+		width: 100%;
+		text-align: center;
+		margin-top: 30px;
+	}
+
+	.overlay .closebtn:hover,
+	.overlay .closebtn:focus {
+		color: #f1f1f1;
+	}
+
+	.overlay .closebtn {
+		position: absolute;
+		right: 45px;
+		font-size: 60px;
+	}
+
+	@media screen and (max-height: 450px) {
+		.overlay .closebtn {
+			font-size: 40px;
+			right: 35px;
+		}
 	}
 </style>
