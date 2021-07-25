1907bcda53ecd6c52d10379d3cd01b231b982c24
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2021-07-25 12:09:31 +0200

    handle inline styles somehow

--- extract-hashes.php
+++ extract-hashes.php
@@ -1,19 +1,14 @@
 <?php
 
-passthru("svelte-kit build");
-passthru("html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --use-short-doctype --minify-css true --input-dir build --output-dir build  --file-ext html");
 
+//passthru("svelte-kit build");
+$min = false;
+if ($min) {
+     passthru("html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --use-short-doctype --minify-css true --input-dir build --output-dir build  --file-ext html");
+}
 $firebase = json_decode(file_get_contents(__DIR__.'/firebase.json'), true);
 $headersSection = &$firebase['hosting']['headers'];
-function setHeader(&$headersSection, $source, $header) {
-    foreach ($headersSection as &$def) {
-        if ($def['source'] == $source) {
-            $def['headers'][0]=$header;
-            return;
-        }
-    }
-    $headersSection[] = [ 'source' => $source, 'headers' => [$header] ];
-}
+$cspHeaderLocation = &$headersSection[0]['headers'];
 
 
 // Construct the iterator
@@ -31,10 +26,18 @@ foreach(new RecursiveIteratorIterator($it) as $file) {
             $key = substr($key, 0, -10);
         }
         //flag s= multiline
-        preg_match_all('#<[^>]*? style=([^>]*)>#si', $content, $matches);
-        foreach ($matches[1] as $inlinestyle) {
-            die("$file: inline style found, not yet supported: $inlinestyle[0]");
-        }
+        $content = preg_replace_callback('#<([^>]*?)(class="(([^"]*?))")([^>]*?)(style="([^>"]*)")([^>]*?)(class="([^"]*)")?([^>]*)>#si', function($matches){
+            $className = substr(md5($matches[7]),0,6);
+            //print ".$className"."{"."$matches[7]}\n";
+            if($matches[3]) {
+                $className .= ' '.$matches[3];
+            }
+            if($matches[10]) {
+                $className .= ' '.$matches[10];
+            }
+            return "<$matches[1]class=\"$className\"$matches[4]$matches[5]$matches[8]$matches[11]>";
+        }, $content);
+        file_put_contents($file, $content);
         preg_match_all('#<script[^>]*>(.*?)</script>#si', $content, $matches);
         foreach ($matches[1] as $js) {
             $hash = "sha256-" . base64_encode(hash('sha256', $js, true));
@@ -53,15 +56,13 @@ $hashes = array_keys($list);
 $scriptHashes = "'" . implode("' '", $hashes) . "'";
 
 
-$svelte_animation_style_hash = 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=';
+$svelte_animation_style_hash = "'unsafe-hashes' 'unsafe-inline'";
 
 $csph = [
     "key"=> "content-security-policy",
-    "value"=> "default-src 'self'; connect-src https://xn--it-sicherheit-fr-unternehmen-g7c.de/; img-src 'self' data:; script-src 'self' $scriptHashes; style-src 'self' '$svelte_animation_style_hash' "
+    "value"=> "default-src 'self'; connect-src https://xn--it-sicherheit-fr-unternehmen-g7c.de/; img-src 'self' data:; script-src 'self' $scriptHashes; style-src 'self' $svelte_animation_style_hash "
 ];
 
-setHeader($headersSection, '*', ['key'=>'X-Frame-Options', 'value'=>'DENY']);
-
-setHeader($headersSection, '*', $csph);
+$cspHeaderLocation[0] = $csph;
 
 file_put_contents(__DIR__.'/firebase.json', json_encode($firebase, JSON_PRETTY_PRINT));
