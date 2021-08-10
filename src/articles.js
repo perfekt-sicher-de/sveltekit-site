45603f3f1ff134a3285788d9d452696306b239b6
Keywan Ghadami <keywan.ghadami@gmail.com>
2021-08-10 08:15:01 +0200

    cloudflare price

--- src/articles.js
+++ src/articles.js
@@ -1,6 +1,7 @@
 {
   "cloudflare_waf": {
      "title": "Cloudflare WAF",
+     "categories" : ["waf_cloudflare"],
      "variants": {
          "pro": {
               "features": [
@@ -8,26 +9,28 @@
                   "Cloudflare Regelset",
                   "Eine Regeln"
                   "ModSecurity Regelset"
-              ]
+              ],
+              "price": "$20"
           },
          "business":{
               "inherit" : "pro",
               "features": [
                   "Priorisierter Kundensupport",
                   "Bot-Analytics und erweiterte Abwehr"
-               ]
+               ],
+               "price": "$200"
          },
          "enterprise":{
                "inherit" : "business",
                "features": [
                   "Priorisierter Kundensupport",
-"Prüfprotokolle und Enterprise-Rohdatenprotokolle mit Zielintegrationen",
-"Analysezeitbereich von 1 Minuten",
-"Priorisierter Kundensupport: rund um die Uhr über Telefon, Chat und E-Mail",
-"SLA mit 100 % Verfügbarkeit und 25-facher Erstattung",
-"Rollenbasierte Kontosteuerung",
-"Single-Sign-On-Support",
-"Netzwerkpriorisierung"
+                  "Prüfprotokolle und Enterprise-Rohdatenprotokolle mit Zielintegrationen",
+                  "Analysezeitbereich von 1 Minuten",
+                  "Priorisierter Kundensupport: rund um die Uhr über Telefon, Chat und E-Mail",
+                  "SLA mit 100 % Verfügbarkeit und 25-facher Erstattung",
+                  "Rollenbasierte Kontosteuerung",
+                  "Single-Sign-On-Support",
+                  "Netzwerkpriorisierung"
                ]
          }
      }
